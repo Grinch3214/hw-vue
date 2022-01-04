3ba226718d29eb1d7695234cbe73fefd969f1897
Maxim Hrichina <scorpiomax90@gmail.com>
2022-01-04 08:48:18 +0200

    Inintial commit

---
+++ src/components/PostPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Hello POST</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint dolor
+      provident distinctio excepturi voluptatibus et aliquid vitae eveniet totam
+      cumque, illo eius reprehenderit minus molestiae magni ullam voluptatum
+      consequatur? Sint?
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PostPage",
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
